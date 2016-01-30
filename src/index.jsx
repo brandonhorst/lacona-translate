164396512a75df0c6c1ddb5b6fb539183a41f916
Brandon Horst <brandonhorst@gmail.com>
2016-01-29 20:31:34 -0500

    Initial beta release

--- src/index.jsx
+++ src/index.jsx
@@ -1,152 +1,242 @@
 /** @jsx createElement */
 
 import _ from 'lodash'
-import URL from 'lacona-phrase-url'
-import String from 'lacona-phrase-string'
-import {createElement, Phrase} from 'lacona-phrase'
+import { URL } from 'lacona-phrase-url'
+import { String } from 'lacona-phrase-string'
+import { createElement, Phrase, Source } from 'lacona-phrase'
+import { openURL } from 'lacona-api'
+import { Command } from 'lacona-command'
+
+class Languages extends Source {
+  data = [
+    {name: 'Afrikaans', code: 'af'},
+    {name: 'Albanian', code: 'sq'},
+    {name: 'Arabic', code: 'ar'},
+    {name: 'Armenian', code: 'hy'},
+    {name: 'Azerbaijani', code: 'az'},
+    {name: 'Basque', code: 'eu'},
+    {name: 'Belarusian', code: 'be'},
+    {name: 'Bengali', code: 'bn'},
+    {name: 'Bosnian', code: 'bs'},
+    {name: 'Bulgarian', code: 'bg'},
+    {name: 'Burmese', code: 'my'},
+    {name: 'Catalan', code: 'ca'},
+    {name: 'Cebuano', code: 'ceb'},
+    {name: 'Chichewa', code: 'ny'},
+    {name: 'Chinese', code: 'zh-CN'},
+    {name: 'Chinese (Simplified)', code: 'zh-CN'},
+    {name: 'Chinese (Traditional)', code: 'zh-TW'},
+    {name: 'Creole', code: 'ht'},
+    {name: 'Croatian', code: 'hr'},
+    {name: 'Czech', code: 'cs'},
+    {name: 'Danish', code: 'da'},
+    {name: 'Dutch', code: 'nl'},
+    {name: 'English', code: 'en'},
+    {name: 'Esperanto', code: 'eo'},
+    {name: 'Estonian', code: 'et'},
+    {name: 'Filipino', code: 'tl'},
+    {name: 'Finnish', code: 'fi'},
+    {name: 'French', code: 'fr'},
+    {name: 'Galician', code: 'gl'},
+    {name: 'Georgian', code: 'ka'},
+    {name: 'German', code: 'de'},
+    {name: 'Greek', code: 'el'},
+    {name: 'Gujarati', code: 'gu'},
+    {name: 'Haitian Creole', code: 'ht'},
+    {name: 'Hausa', code: 'ha'},
+    {name: 'Hebrew', code: 'iw'},
+    {name: 'Hindi', code: 'hi'},
+    {name: 'Hmong', code: 'hmn'},
+    {name: 'Hungarian', code: 'hu'},
+    {name: 'Farsi', code: 'fa'},
+    {name: 'Icelandic', code: 'is'},
+    {name: 'Igbo', code: 'ig'},
+    {name: 'Indonesian', code: 'id'},
+    {name: 'Irish', code: 'ga'},
+    {name: 'Italian', code: 'it'},
+    {name: 'Japanese', code: 'ja'},
+    {name: 'Javanese', code: 'jw'},
+    {name: 'Kannada', code: 'kn'},
+    {name: 'Kazakh', code: 'kk'},
+    {name: 'Khmer', code: 'km'},
+    {name: 'Korean', code: 'ko'},
+    {name: 'Lao', code: 'lo'},
+    {name: 'Latin', code: 'la'},
+    {name: 'Latvian', code: 'lv'},
+    {name: 'Lithuanian', code: 'lt'},
+    {name: 'Macedonian', code: 'mk'},
+    {name: 'Malagasy', code: 'mg'},
+    {name: 'Malay', code: 'ms'},
+    {name: 'Malayalam', code: 'ml'},
+    {name: 'Maltese', code: 'mt'},
+    {name: 'Maori', code: 'mi'},
+    {name: 'Marathi', code: 'mr'},
+    {name: 'Mongolian', code: 'mn'},
+    {name: 'Myanmar', code: 'my'},
+    {name: 'Nepali', code: 'ne'},
+    {name: 'Norwegian', code: 'no'},
+    {name: 'Persian', code: 'fa'},
+    {name: 'Polish', code: 'pl'},
+    {name: 'Portuguese', code: 'pt'},
+    {name: 'Punjabi', code: 'pa'},
+    {name: 'Romanian', code: 'ro'},
+    {name: 'Russian', code: 'ru'},
+    {name: 'Serbian', code: 'sr'},
+    {name: 'Sesotho', code: 'st'},
+    {name: 'Simplified Chinese', code: 'zh-CN'},
+    {name: 'Sinhala', code: 'si'},
+    {name: 'Slovak', code: 'sk'},
+    {name: 'Slovenian', code: 'sl'},
+    {name: 'Somali', code: 'so'},
+    {name: 'Spanish', code: 'es'},
+    {name: 'Sundanese', code: 'su'},
+    {name: 'Swahili', code: 'sw'},
+    {name: 'Swedish', code: 'sv'},
+    {name: 'Tajik', code: 'tg'},
+    {name: 'Tamil', code: 'ta'},
+    {name: 'Telugu', code: 'te'},
+    {name: 'Traditional Chinese', code: 'zh-TW'},
+    {name: 'Thai', code: 'th'},
+    {name: 'Turkish', code: 'tr'},
+    {name: 'Ukrainian', code: 'uk'},
+    {name: 'Urdu', code: 'ur'},
+    {name: 'Uzbek', code: 'uz'},
+    {name: 'Vietnamese', code: 'vi'},
+    {name: 'Welsh', code: 'cy'},
+    {name: 'Yiddish', code: 'yi'},
+    {name: 'Yoruba', code: 'yo'},
+    {name: 'Zulu', code: 'zu'} 
+  ]
+}
 
 class Language extends Phrase {
+  observe () {
+    return <Languages />
+  }
+
   describe () {
+    const sourceItems = _.map(this.source.data, ({name, code}) => ({
+      text: name,
+      value: {name, code}
+    }))
+
     return (
-      <argument text='language'>
-        <list items={[
-          {text: 'Afrikaans', value: {name: 'Afrikaans', code: 'af'}},
-          {text: 'Albanian', value: {name: 'Albanian', code: 'sq'}},
-          {text: 'Arabic', value: {name: 'Arabic', code: 'ar'}},
-          {text: 'Armenian', value: {name: 'Armenian', code: 'hy'}},
-          {text: 'Azerbaijani', value: {name: 'Azerbaijani', code: 'az'}},
-          {text: 'Basque', value: {name: 'Basque', code: 'eu'}},
-          {text: 'Belarusian', value: {name: 'Belarusian', code: 'be'}},
-          {text: 'Bengali', value: {name: 'Bengali', code: 'bn'}},
-          {text: 'Bosnian', value: {name: 'Bosnian', code: 'bs'}},
-          {text: 'Bulgarian', value: {name: 'Bulgarian', code: 'bg'}},
-          {text: 'Burmese', value: {name: 'Burmese', code: 'my'}},
-          {text: 'Catalan', value: {name: 'Catalan', code: 'ca'}},
-          {text: 'Cebuano', value: {name: 'Cebuano', code: 'ceb'}},
-          {text: 'Chichewa', value: {name: 'Chichewa', code: 'ny'}},
-          {text: 'Chinese', value: {name: 'Chinese', code: 'zh-CN'}},
-          {text: 'Chinese (Simplified)', value: {name: 'Chinese (Simplified)', code: 'zh-CN'}},
-          {text: 'Chinese (Traditional)', value: {name: 'Chinese (Traditional)', code: 'zh-TW'}},
-          {text: 'Croatian', value: {name: 'Croatian', code: 'hr'}},
-          {text: 'Czech', value: {name: 'Czech', code: 'cs'}},
-          {text: 'Danish', value: {name: 'Danish', code: 'da'}},
-          {text: 'Dutch', value: {name: 'Dutch', code: 'nl'}},
-          {text: 'English', value: {name: 'English', code: 'en'}},
-          {text: 'Esperanto', value: {name: 'Esperanto', code: 'eo'}},
-          {text: 'Estonian', value: {name: 'Estonian', code: 'et'}},
-          {text: 'Filipino', value: {name: 'Filipino', code: 'tl'}},
-          {text: 'Finnish', value: {name: 'Finnish', code: 'fi'}},
-          {text: 'French', value: {name: 'French', code: 'fr'}},
-          {text: 'Galician', value: {name: 'Galician', code: 'gl'}},
-          {text: 'Georgian', value: {name: 'Georgian', code: 'ka'}},
-          {text: 'German', value: {name: 'German', code: 'de'}},
-          {text: 'Greek', value: {name: 'Greek', code: 'el'}},
-          {text: 'Gujarati', value: {name: 'Gujarati', code: 'gu'}},
-          {text: 'Haitian Creole', value: {name: 'Haitian Creole', code: 'ht'}},
-          {text: 'Hausa', value: {name: 'Hausa', code: 'ha'}},
-          {text: 'Hebrew', value: {name: 'Hebrew', code: 'iw'}},
-          {text: 'Hindi', value: {name: 'Hindi', code: 'hi'}},
-          {text: 'Hmong', value: {name: 'Hmong', code: 'hmn'}},
-          {text: 'Hungarian', value: {name: 'Hungarian', code: 'hu'}},
-          {text: 'Farsi', value: {name: 'Farsi', code: 'fa'}},
-          {text: 'Icelandic', value: {name: 'Icelandic', code: 'is'}},
-          {text: 'Igbo', value: {name: 'Igbo', code: 'ig'}},
-          {text: 'Indonesian', value: {name: 'Indonesian', code: 'id'}},
-          {text: 'Irish', value: {name: 'Irish', code: 'ga'}},
-          {text: 'Italian', value: {name: 'Italian', code: 'it'}},
-          {text: 'Japanese', value: {name: 'Japanese', code: 'ja'}},
-          {text: 'Javanese', value: {name: 'Javanese', code: 'jw'}},
-          {text: 'Kannada', value: {name: 'Kannada', code: 'kn'}},
-          {text: 'Kazakh', value: {name: 'Kazakh', code: 'kk'}},
-          {text: 'Khmer', value: {name: 'Khmer', code: 'km'}},
-          {text: 'Korean', value: {name: 'Korean', code: 'ko'}},
-          {text: 'Lao', value: {name: 'Lao', code: 'lo'}},
-          {text: 'Latin', value: {name: 'Latin', code: 'la'}},
-          {text: 'Latvian', value: {name: 'Latvian', code: 'lv'}},
-          {text: 'Lithuanian', value: {name: 'Lithuanian', code: 'lt'}},
-          {text: 'Macedonian', value: {name: 'Macedonian', code: 'mk'}},
-          {text: 'Malagasy', value: {name: 'Malagasy', code: 'mg'}},
-          {text: 'Malay', value: {name: 'Malay', code: 'ms'}},
-          {text: 'Malayalam', value: {name: 'Malayalam', code: 'ml'}},
-          {text: 'Maltese', value: {name: 'Maltese', code: 'mt'}},
-          {text: 'Maori', value: {name: 'Maori', code: 'mi'}},
-          {text: 'Marathi', value: {name: 'Marathi', code: 'mr'}},
-          {text: 'Mongolian', value: {name: 'Mongolian', code: 'mn'}},
-          {text: 'Myanmar', value: {name: 'Myanmar', code: 'my'}},
-          {text: 'Nepali', value: {name: 'Nepali', code: 'ne'}},
-          {text: 'Norwegian', value: {name: 'Norwegian', code: 'no'}},
-          {text: 'Persian', value: {name: 'Persian', code: 'fa'}},
-          {text: 'Polish', value: {name: 'Polish', code: 'pl'}},
-          {text: 'Portuguese', value: {name: 'Portuguese', code: 'pt'}},
-          {text: 'Punjabi', value: {name: 'Punjabi', code: 'pa'}},
-          {text: 'Romanian', value: {name: 'Romanian', code: 'ro'}},
-          {text: 'Russian', value: {name: 'Russian', code: 'ru'}},
-          {text: 'Serbian', value: {name: 'Serbian', code: 'sr'}},
-          {text: 'Sesotho', value: {name: 'Sesotho', code: 'st'}},
-          {text: 'Sinhala', value: {name: 'Sinhala', code: 'si'}},
-          {text: 'Slovak', value: {name: 'Slovak', code: 'sk'}},
-          {text: 'Slovenian', value: {name: 'Slovenian', code: 'sl'}},
-          {text: 'Somali', value: {name: 'Somali', code: 'so'}},
-          {text: 'Spanish', value: {name: 'Spanish', code: 'es'}},
-          {text: 'Sundanese', value: {name: 'Sundanese', code: 'su'}},
-          {text: 'Swahili', value: {name: 'Swahili', code: 'sw'}},
-          {text: 'Swedish', value: {name: 'Swedish', code: 'sv'}},
-          {text: 'Tajik', value: {name: 'Tajik', code: 'tg'}},
-          {text: 'Tamil', value: {name: 'Tamil', code: 'ta'}},
-          {text: 'Telugu', value: {name: 'Telugu', code: 'te'}},
-          {text: 'Thai', value: {name: 'Thai', code: 'th'}},
-          {text: 'Turkish', value: {name: 'Turkish', code: 'tr'}},
-          {text: 'Ukrainian', value: {name: 'Ukrainian', code: 'uk'}},
-          {text: 'Urdu', value: {name: 'Urdu', code: 'ur'}},
-          {text: 'Uzbek', value: {name: 'Uzbek', code: 'uz'}},
-          {text: 'Vietnamese', value: {name: 'Vietnamese', code: 'vi'}},
-          {text: 'Welsh', value: {name: 'Welsh', code: 'cy'}},
-          {text: 'Yiddish', value: {name: 'Yiddish', code: 'yi'}},
-          {text: 'Yoruba', value: {name: 'Yoruba', code: 'yo'}},
-          {text: 'Zulu', value: {name: 'Zulu', code: 'zu'}}
-        ]} />
-      </argument>
+      <label text='language'>
+        <list limit={10} items={sourceItems} />
+      </label>
     )
   }
 }
 
-
-export function execute (result) {
-  const toList = result.to || [{name: 'English', code: 'en'}]
-  const from = (result.from && result.from.code) || 'auto'
-
-  _.forEach(toList, to => {
-    const body = encodeURI(result.phrase || result.url)
-    const url = `https://translate.google.com/#${from}/${to.code}/${body}`
-    global.openURL(url)
-  })
+function andify (array, separator = ',') {
+  if (array.length === 1) {
+    return {text: array[0].name, argument: 'language'}
+  } else {
+    return _.chain(array)
+      .slice(0, -2)
+      .map(item => [{text: item.name, argument: 'language'}, {text: ', '}])
+      .flatten()
+      .concat({text: _.slice(array, -2, -1)[0].name, argument: 'language'})
+      .concat({text: ' and '})
+      .concat({text: _.slice(array, -1)[0].name, argument: 'language'})
+      .value()
+  }
 }
 
+class CommandObject {
+  constructor ({
+      to = [{name: 'English', code: 'en'}],
+      from = {name: 'an automatically detected language', code: 'auto'},
+      phrase,
+      url}) {
+    this.to = to
+    this.from = from
+    this.phrase = phrase
+    this.url = url
+  }
 
-export class Sentence extends Phrase {
+  _demoExecute () {
+    let result
+
+    if (this.to.length === 1) {
+      if (this.phrase) {
+        result = [
+          {text: 'a Google translation of '},
+          {text: this.phrase, argument: 'phrase'}
+        ]
+      } else if (this.url) {
+        result = [
+          {text: 'version of '},
+          {text: this.url, argument: 'url'},
+          {text: ' Google translated'}
+        ]
+      }
+    } else {
+      if (this.phrase) {
+        result = [
+          {text: 'Google translations of '},
+          {text: this.phrase, argument: 'phrase'}
+        ]
+      } else if (this.url) {
+        result = [
+          {text: 'Google translated versions of '},
+          {text: this.url, argument: 'url'}
+        ]
+      }
+    }
+
+    return _.flatten([
+      {text: 'open ', category: 'action'},
+      result,
+      {text: ' from '},
+      {text: this.from.name, argument: 'language'},
+      {text: ' to '},
+      andify(this.to),
+      {text: ' in '},
+      {text: 'the default browser', argument: 'application'}
+    ])
+  }
+
+  execute () {
+    _.forEach(this.to, (to) => {
+      let url
+      if (this.phrase) {
+        url = `https://translate.google.com/#${this.from.code}/${to.code}/${encodeURIComponent(this.phrase)}`
+      } else if (this.url) {
+        url = `https://translate.google.com/translate?sl=${this.from.code}&tl=${to.code}&u=${encodeURIComponent(this.url)}`
+      }
+
+      openURL({url})
+    })
+  }
+}
+
+export class Translate extends Phrase {
+  static extends = [Command]
+
   describe () {
     return (
-      <sequence>
-        <literal text='translate ' category='action' />
-        <choice merge={true}>
-          <URL id='url' />
-          <String argument='phrase' id='phrase' limit={1} splitOn=' ' />
-        </choice>
-        <sequence optional={true} merge={true}>
-          <literal text=' from ' category='conjunction' />
-          <Language id='from' />
+      <map function={result => new CommandObject(result)}>
+        <sequence>
+          <literal text='translate ' category='action' />
+          <choice merge={true}>
+            <URL id='url' splitOn=' ' />
+            <String argument='phrase' id='phrase' limit={1} splitOn=' ' />
+          </choice>
+          <sequence optional={true} merge={true}>
+            <literal text=' from ' category='conjunction' />
+            <Language id='from' />
+          </sequence>
+          <sequence optional={true} merge={true}>
+            <literal text=' to ' />
+            <repeat separator={<list items={[', ', ' and ', ', and ']} category='conjunction' limit={1} />} id='to' unique>
+              <Language />
+            </repeat>
+          </sequence>
         </sequence>
-        <sequence optional={true} merge={true}>
-          <literal text=' to ' />
-          <repeat separator={<list items={[', ', ' and ', ', and ']} category='conjunction' limit={1} />} id='to' unique={true}>
-            <Language />
-          </repeat>
-        </sequence>
-      </sequence>
+      </map>
     )
   }
 }
 
-export default {
-  sentences: [{Sentence, execute}]
-}
+export const extensions = [Translate]
